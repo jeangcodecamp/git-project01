965488ee0ec664150b4bba3ad1e429ff9b32e73e
tratchapong <tratchapong@gmail.com>
2024-06-18 14:16:04 +0700

    start server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+
+
+app.use((req, res,next)=>{
+  res.status(404).json({ msg : 'resource not found'})
+})
+
+app.use((err,req,res,next)=> {
+  res.json({err : err.message})
+})
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=> console.log('Server on',port))
