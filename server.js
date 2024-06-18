8734cab94929c6e406f31f462b085ae9102d3124
tratchapong <tratchapong@gmail.com>
2024-06-18 14:51:38 +0700

    not-found middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const notFound = require('./middlewares/not-found')
 const app = express()
 
 app.use(cors())
@@ -8,9 +9,9 @@ app.use(express.json())
 
 
 
-app.use((req, res,next)=>{
-  res.status(404).json({ msg : 'resource not found'})
-})
+
+
+app.use(notFound)
 
 app.use((err,req,res,next)=> {
   res.json({err : err.message})
