95df7f48c552519e1e4363995b50cf15bc6f232a
tratchapong <tratchapong@gmail.com>
2024-06-18 14:58:28 +0700

    error middleware done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const errorMiddleware = require('./middlewares/error-middleware')
 const app = express()
 
 app.use(cors())
@@ -12,9 +13,7 @@ app.use((req, res,next)=>{
   res.status(404).json({ msg : 'resource not found'})
 })
 
-app.use((err,req,res,next)=> {
-  res.json({err : err.message})
-})
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=> console.log('Server on',port))
